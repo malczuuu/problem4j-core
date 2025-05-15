7e03fafcc703b9fa4a04ee6ff0ee90dc7f3ce396
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-05-15 15:01:19 +0200

    Update ProblemException message generation

--- src/main/java/io/github/malczuuu/problem4j/core/ProblemException.java
+++ src/main/java/io/github/malczuuu/problem4j/core/ProblemException.java
@@ -1,21 +1,59 @@
 package io.github.malczuuu.problem4j.core;
 
+import java.io.Serial;
+
 public class ProblemException extends RuntimeException {
+
+  @Serial private static final long serialVersionUID = 1L;
 
   private final Problem problem;
 
   public ProblemException(Problem problem) {
-    super(emptyIfNull(problem.getDetail()));
+    super(produceExceptionMessage(problem));
     this.problem = problem;
   }
 
-  private static String emptyIfNull(String str) {
-    return str != null ? str : "";
+  public ProblemException(String message, Problem problem) {
+    super(message);
+    this.problem = problem;
   }
 
   public ProblemException(Problem problem, Throwable cause) {
-    super(emptyIfNull(problem.getDetail()), cause);
+    super(produceExceptionMessage(problem), cause);
     this.problem = problem;
+  }
+
+  public ProblemException(String message, Problem problem, Throwable cause) {
+    super(message, cause);
+    this.problem = problem;
+  }
+
+  private static String produceExceptionMessage(Problem problem) {
+    StringBuilder builder = new StringBuilder();
+
+    if (problem.getTitle() != null) {
+      builder.append(problem.getTitle());
+    }
+
+    if (problem.getDetail() != null) {
+      if (!builder.isEmpty()) {
+        builder.append(": ");
+      }
+      builder.append(problem.getDetail());
+    }
+
+    if (problem.getStatus() != 0) {
+      if (!builder.isEmpty()) {
+        builder.append(" ");
+      }
+      builder.append("(code: ").append(problem.getStatus()).append(")");
+    }
+
+    if (builder.isEmpty()) {
+      return null;
+    }
+
+    return builder.toString();
   }
 
   public Problem getProblem() {
