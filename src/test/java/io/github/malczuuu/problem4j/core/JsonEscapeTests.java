10cdcebb90a6a1b7ae1489fd2c27fdbbe41638b5
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-09-20 12:31:18 +0200

    Refactor JsonEscapeTests to use @CsvSource

--- src/test/java/io/github/malczuuu/problem4j/core/JsonEscapeTests.java
+++ src/test/java/io/github/malczuuu/problem4j/core/JsonEscapeTests.java
@@ -3,34 +3,24 @@ package io.github.malczuuu.problem4j.core;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class JsonEscapeTests {
 
-  private static class EscapingData {
-
-    public static EscapingData from(String string) {
-      String[] splited = string.split(",");
-      return new EscapingData(splited[0], splited[1]);
-    }
-
-    private final String given;
-    private final String expected;
-
-    private EscapingData(String given, String expected) {
-      this.given = given;
-      this.expected = expected;
-    }
-  }
-
   @ParameterizedTest
-  @ValueSource(
-      strings = {"\",\\\"", "\\,\\\\", "\b,\\b", "\f,\\f", "\n,\\n", "\r,\\r", "\t,\\t", "/,\\/"})
-  void shouldEscapeSpecialCharactersWithSlash(String value) {
-    EscapingData data = EscapingData.from(value);
+  @CsvSource({
+    "'\"','\\\"','quote'",
+    "'\\','\\\\','backslash'",
+    "'\b','\\b','backspace'",
+    "'\f','\\f','formfeed'",
+    "'\n','\\n','newline'",
+    "'\r','\\r','carriage return'",
+    "'\t','\\t','tab'",
+    "'/','\\/','slash'",
+  })
+  void shouldEscapeSpecialCharactersWithSlash(String given, String expected, String name) {
+    String result = JsonEscape.escape(given);
 
-    String result = JsonEscape.escape(data.given);
-
-    assertEquals(data.expected, result, "Failure in escaping \"" + data.expected + "\"");
+    assertEquals(expected, result, name + " (\"" + expected + "\") failed with \"" + result + "\"");
   }
 }
