baa2b45409ca7619932d932b4feae9502c18f451
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-10-01 22:04:15 +0200

    Add assertj dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,8 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("org.assertj:assertj-core:3.27.6")
 }
 
 publishing {
