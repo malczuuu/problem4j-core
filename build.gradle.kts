826ef01019cd2ee195d24fc18b08ae23c564c3ab
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-10-25 13:35:06 +0200

    Manage dependencies with libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,12 +31,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation(platform(libs.junit.bom))
 
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
 
-    testImplementation("org.assertj:assertj-core:3.27.6")
+    testImplementation(libs.assertj.core)
 }
 
 publishing {
