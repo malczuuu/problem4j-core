67d3c8e1f53b0b2079169d4ba73107723619e81d
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-11-16 00:43:56 +0100

    Bump nmcp to 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("maven-publish")
     id("signing")
     id("com.diffplug.spotless").version("8.0.0")
-    id("com.gradleup.nmcp").version("1.2.0")
+    id("com.gradleup.nmcp").version("1.2.1")
 }
 
 group = "io.github.malczuuu.problem4j"
