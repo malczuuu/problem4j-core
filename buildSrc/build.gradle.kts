1989b1016b7314a4d63a6f9aab91546173cbd69e
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-13 17:52:29 +0100

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `kotlin-dsl`
 }
 
-// Kotlin does not yet support 25 JDK target, to be revisited in the future.
+// To hide warnings that Kotlin does not support 25 JDK target yet, to be revisited in the future.
 if (JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_25)) {
     kotlin {
         jvmToolchain {
