ef8d856479c41f3743a8f63f9a25e5bdc4b0a86e
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-20 12:01:46 +0100

    Update registration of printVersion task

--- build.gradle.kts
+++ build.gradle.kts
@@ -152,12 +152,12 @@ tasks.register<Delete>("cleanLibs") {
 
 // Usage:
 //   ./gradlew printVersion
-tasks.register("printVersion") {
-    description = "Prints the current project version to the console"
+tasks.register<DefaultTask>("printVersion") {
+    description = "Prints the current project version to the console."
     group = "help"
 
     val projectName = project.name
-    val projectVersion = project.version
+    val projectVersion = project.version.toString()
 
     doLast {
         println("$projectName version: $projectVersion")
