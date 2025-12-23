fa5335ec7e0379f603e8a24524420438f7c52ff2
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-12-23 20:05:01 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,8 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")
 }
 
-// if rootProject.name matches project.name, it fails to generate output for nmcpZipAggregation task
-rootProject.name = "problem4j-root"
+// if rootProject.name matches project.name, it fails to generate output for nmcpZipAggregation task, it's probably
+// because there's only one submodule
+rootProject.name = "problem4j-core-root"
 
 include(":problem4j-core")
